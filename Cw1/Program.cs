e01082a16e3ee76504a0dd01cfe3020053837e07
Oleksandr Rudenko <s19434@pjwstk.edu.pl>
2020-03-25 05:11:52 +0100

    End fix

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -9,31 +9,47 @@ namespace Cwiczenia1
     {
         public static async Task Main(string[] args)
         {
-            var url = "https://www.pja.edu.pl";
+            var url = "https://www.pja.edu.p";
 
             var client = new HttpClient();
             var result = await client.GetAsync(url);
 
-            if (result.IsSuccessStatusCode)
+            try
             {
 
-                string html = await result.Content.ReadAsStringAsync();
-
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",
-                    RegexOptions.IgnoreCase);
-
-                var matches = regex.Matches(html);
-                foreach (var m in matches)
+                if (result.IsSuccessStatusCode)
                 {
-                    Console.WriteLine(m);
+
+                    string html = await result.Content.ReadAsStringAsync();
+
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",
+                        RegexOptions.IgnoreCase);
+
+                    var matches = regex.Matches(html);
+                    foreach (var m in matches)
+                    {
+                        Console.WriteLine(m);
+                    }
+
+
                 }
 
 
+                Console.WriteLine("Hello World!");
+            }catch(InvalidOperationException e)
+            {
+                throw new ArgumentException("Wrong adress");
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Error while requesting");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
 
-
-            Console.WriteLine("Hello World!");
-
+            client.Dispose();
 
 
         }
