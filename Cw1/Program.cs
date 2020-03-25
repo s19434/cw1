c4a9882440ba0466d860b5716a417a3e0ed7702e
Oleksandr Rudenko <s19434@pjwstk.edu.pl>
2020-03-25 03:39:39 +0100

    First commit

---
+++ Cw1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var url = "https://www.pja.edu.pl";
+
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+
+            if (result.IsSuccessStatusCode)
+            {
+
+                string html = await result.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",
+                    RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(html);
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+
+
+            }
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
